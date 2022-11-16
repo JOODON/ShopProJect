309276a061fdc801392778271fe1ebc0263fb82f
주동호 <100278301+JOODON@users.noreply.github.com>
2022-11-16 09:46:15 +0900

    Test코드 작성 및 다른 JPA 쿼리문 작성!

--- src/main/java/com/ShopProJect/ShopProJect/repository/ItemRepository.java
+++ src/main/java/com/ShopProJect/ShopProJect/repository/ItemRepository.java
@@ -10,6 +10,12 @@ public interface ItemRepository extends JpaRepository<Item,Long> {
     //두번쨰 제네릭 타입에는 키본키 타입을 넣어주면됨!
 
     List<Item> findByItemNm(String itemNm);
+    //이름으로 찾기
     List<Item> findByItemNmOrItemDetail(String itemNm,String itemDetail);
-
+    //이름이랑 상세 설명으로 찾기
+    List<Item> findByPriceLessThan(Integer price);
+    //가격보다 적은 가격으로 찾기
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+    //가격을 내림차순으로 조회
+    //Asc는 오름차순 Desc는 내림차순!
 }
--- src/test/java/com/ShopProJect/ShopProJect/ShopProJectApplicationTests.java
+++ src/test/java/com/ShopProJect/ShopProJect/ShopProJectApplicationTests.java
@@ -15,43 +15,74 @@ import java.util.List;
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
 class ShopProJectApplicationTests {
-	@Autowired
-	ItemRepository itemRepository;
-	@Test
-	public void createItemList() {
-		for (int i=0; i<10; i++){
-		Item item=new Item();
-		item.setItemNm("테스트 상품"+i);
-		item.setPrice(10000+i);
-		item.setItemDetail("테스트 상품 상세 설명"+i);
-		item.setItemSellStatus(ItemSellStatus.SELL);
-		item.setStockNumber(100);
-		item.setRegTime(LocalDateTime.now());
-		item.setUpdateTime(LocalDateTime.now());
-		Item saveItem=itemRepository.save(item);
-		}
+			@Autowired
+			ItemRepository itemRepository;
+			@Test
+			public void createItemList() {
+				for (int i=0; i<10; i++){
+				Item item=new Item();
+				item.setItemNm("테스트 상품"+i);
+				item.setPrice(10000+i);
+				item.setItemDetail("테스트 상품 상세 설명"+i);
+				item.setItemSellStatus(ItemSellStatus.SELL);
+				item.setStockNumber(100);
+				item.setRegTime(LocalDateTime.now());
+				item.setUpdateTime(LocalDateTime.now());
+				Item saveItem=itemRepository.save(item);
+				}
+			}
 
-	}
-	@Test
-	@DisplayName("상품명 조회 테스트")
-	public void findByItemTest() {
-		this.createItemList();
-		List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
-		for (Item item : itemList) {
-			System.out.println(item.toString());
-		}
-	}
-	//Item(id=2, itemNm=테스트 상품1, price=10001, stockNumber=100, itemDetail=테스트 상품 상세 설명1, itemSellStatus=SELL, regTime=2022-11-15T21:38:06.708535, updateTime=2022-11-15T21:38:06.708535)
+			@Test
+			@DisplayName("상품명 조회 테스트")
+			public void findByItemTest() {
+				this.createItemList();
+				List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
+				for (Item item : itemList) {
+					System.out.println(item.toString());
+				}
+			}
+		//Item(id=2, itemNm=테스트 상품1, price=10001, stockNumber=100, itemDetail=테스트 상품 상세 설명1, itemSellStatus=SELL, regTime=2022-11-15T21:38:06.708535, updateTime=2022-11-15T21:38:06.708535)
 
-		@Test
-		@DisplayName("상품명 ,상품 상세설명 or 테스트")
-		public void findByItemNmOrItemDetailTest() {
-			this.createItemList();
-			List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1","테스트 상품 상세 설명5");
-			for (Item item : itemList) {
-				System.out.println(item.toString());
+			@Test
+			@DisplayName("상품명 ,상품 상세설명 or 테스트")
+			public void findByItemNmOrItemDetailTest() {
+				this.createItemList();
+				List<Item> itemList = itemRepository.findByItemNmOrItemDetail("테스트 상품1", "테스트 상품 상세 설명5");
+				for (Item item : itemList) {
+					System.out.println(item.toString());
+				}
 			}
 			//Item(id=2, itemNm=테스트 상품1, price=10001, stockNumber=100, itemDetail=테스트 상품 상세 설명1, itemSellStatus=SELL, regTime=2022-11-15T21:37:12.914731, updateTime=2022-11-15T21:37:12.914731)
-			//Item(id=6, itemNm=테스트 상품5, price=10005, stockNumber=100, itemDetail=테스트 상품 상세 설명5, itemSellStatus=SELL, regTime=2022-11-15T21:37:12.924715, updateTime=2022-11-15T21:37:12.924715)
-	}
+			//Item(id=6, itemNm=테스트 상품5, price=10005, stockNumber=100, itemDetail=테스트 상품T 상세 설명5, itemSellStatus=SELL, regTime=2022-11-15T21:37:12.924715, updateTime=2022-11-15T21:37:12.924715)
+
+			@Test
+			@DisplayName("가격 LessThen Test")
+			public void findByPriceLessThenTest(){
+				this.createItemList();
+				List<Item> itemList=itemRepository.findByPriceLessThan(10005);
+				for (Item item : itemList) {
+					System.out.println(item.toString());
+				}
+			//Item(id=1, itemNm=테스트 상품0, price=10000, stockNumber=100, itemDetail=테스트 상품 상세 설명0, itemSellStatus=SELL, regTime=2022-11-16T09:08:15.758644, updateTime=2022-11-16T09:08:15.758644)
+			//Item(id=2, itemNm=테스트 상품1, price=10001, stockNumber=100, itemDetail=테스트 상품 상세 설명1, itemSellStatus=SELL, regTime=2022-11-16T09:08:15.936706, updateTime=2022-11-16T09:08:15.936706)
+			//Item(id=3, itemNm=테스트 상품2, price=10002, stockNumber=100, itemDetail=테스트 상품 상세 설명2, itemSellStatus=SELL, regTime=2022-11-16T09:08:15.941708, updateTime=2022-11-16T09:08:15.941708)
+			//Item(id=4, itemNm=테스트 상품3, price=10003, stockNumber=100, itemDetail=테스트 상품 상세 설명3, itemSellStatus=SELL, regTime=2022-11-16T09:08:15.948714, updateTime=2022-11-16T09:08:15.948714)
+			//Item(id=5, itemNm=테스트 상품4, price=10004, stockNumber=100, itemDetail=테스트 상품 상세 설명4, itemSellStatus=SELL, regTime=2022-11-16T09:08:15.956716, updateTime=2022-11-16T09:08:15.956716)
+			}
+			@Test
+			@DisplayName("가격 LessThen 내림 차순 정렬 Test")
+			public void findByPriceLessThenOrderByDescTest() {
+				this.createItemList();
+				List<Item> itemList = itemRepository.findByPriceLessThanOrderByPriceDesc(10005);
+				for (Item item : itemList) {
+					System.out.println(item.toString());
+				}
+			//Item(id=5, itemNm=테스트 상품4, price=10004, stockNumber=100, itemDetail=테스트 상품 상세 설명4, itemSellStatus=SELL, regTime=2022-11-16T09:16:37.546793, updateTime=2022-11-16T09:16:37.546793)
+			//Item(id=4, itemNm=테스트 상품3, price=10003, stockNumber=100, itemDetail=테스트 상품 상세 설명3, itemSellStatus=SELL, regTime=2022-11-16T09:16:37.541791, updateTime=2022-11-16T09:16:37.541791)
+			//Item(id=3, itemNm=테스트 상품2, price=10002, stockNumber=100, itemDetail=테스트 상품 상세 설명2, itemSellStatus=SELL, regTime=2022-11-16T09:16:37.536790, updateTime=2022-11-16T09:16:37.536790)
+			//Item(id=2, itemNm=테스트 상품1, price=10001, stockNumber=100, itemDetail=테스트 상품 상세 설명1, itemSellStatus=SELL, regTime=2022-11-16T09:16:37.531789, updateTime=2022-11-16T09:16:37.531789)
+			//Item(id=1, itemNm=테스트 상품0, price=10000, stockNumber=100, itemDetail=테스트 상품 상세 설명0, itemSellStatus=SELL, regTime=2022-11-16T09:16:37.364613, updateTime=2022-11-16T09:16:37.364613)
+			}
 }
+
+
