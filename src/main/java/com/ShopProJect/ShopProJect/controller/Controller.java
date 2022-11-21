ee098545258c7a678e0fc0dd9938359f821998c3
주동호 <100278301+JOODON@users.noreply.github.com>
2022-11-21 21:20:12 +0900

    타임리프문 each연습!

--- src/main/java/com/ShopProJect/ShopProJect/controller/Controller.java
+++ src/main/java/com/ShopProJect/ShopProJect/controller/Controller.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @org.springframework.stereotype.Controller
 @RequestMapping(value = "/thymeleaf")
@@ -25,5 +27,22 @@ public class Controller {
 
         model.addAttribute("itemDto",itemDto);
         return "thymeleafEx/thymeleafEx02";
+    }
+    @GetMapping(value = "/ex03")
+    public String thymeleafExample03(Model model) {
+        List<ItemDto> itemDtoList=new ArrayList<>();
+
+        for(int i=0; i<=10 ; i++){
+            ItemDto itemDto=new ItemDto();
+
+            itemDto.setItemDetail("상세 설명 설명"+i);
+            itemDto.setItemNm("테스트 상품"+i);
+            itemDto.setPrice(1000*i);
+            itemDto.setRegTime(LocalDateTime.now());
+
+            itemDtoList.add(itemDto);
+        }
+        model.addAttribute("itemDtoList",itemDtoList);
+        return "thymeleafEx/thymeleafEx03";
     }
 }
